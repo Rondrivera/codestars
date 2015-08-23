b7f40695e82a633122a274ae240fded68d44c4d3
Ron Rivera <Ron.drivera@gmail.com>
2015-08-23 08:13:26 -0400

    initial commit

---
+++ codestars/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  codestars
+//
+//  Created by Ronald Rivera on 7/27/15.
+//  Copyright (c) 2015 Ronald Rivera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
